1ae271b6b23758161666a525cedf02322d16c150
Priscila Oliveira Alberton <145785226+Priscilaoliveiraalberton@users.noreply.github.com>
2025-06-24 07:42:57 -0300

    24/06

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-09T14:00:00");
 const tempoObjetivo2 = new Date("2025-10-22T15:00:00");
-const tempoObjetivo3 = new Date("2026-2-100T20:00:00");
+const tempoObjetivo3 = new Date("2026-06-10T14:00:00");
 const tempoObjetivo4 = new Date("2026-06-10T14:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
